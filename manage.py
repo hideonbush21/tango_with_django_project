6da9f353be4809c9b6cfb80c45ab29e25a05d824
hideonbush21 <1254408289@qq.com>
2021-07-29 23:47:21 +0100

    chapter 4

--- manage.py
+++ manage.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
-def main():
-    """Run administrative tasks."""
+if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,6 +14,3 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
-    main()
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,4 +1,5 @@
 """tango_with_django_project URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
@@ -16,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     # 3 - The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
