56cd0d433476fce30e7106d993327519e8e14e06
hideonbush21 <1254408289@qq.com>
2021-07-30 01:52:27 +0100

    chapter 4

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == '__main__':
+
+def main():
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,3 +17,6 @@ if __name__ == '__main__':
         ) from exc
     execute_from_command_line(sys.argv)
 
+
+if __name__ == '__main__':
+    main()
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,7 +1,7 @@
 """tango_with_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    # 3 - The above maps any URLs starting with rango/ to be handled by rango.
+# The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
